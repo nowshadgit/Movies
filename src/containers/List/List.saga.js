b43917f1badef8cb413fa9c89e97cbde70b31f99
Nowshad Shaik <nowshad@mobiux.in>
2020-10-14 00:43:33 +0530

    small bug has been fixed.

--- src/containers/List/List.saga.js
+++ src/containers/List/List.saga.js
@@ -36,34 +36,41 @@ function* getMoviesWithId(action) {
     const { payload } = action;
     
     let allReq = []
-    for(let each of payload){
-      const url = `https://www.omdbapi.com/?i=${each}&apikey=a07e499a`
-      allReq.push(call(getData,url))
-    }
-    response = yield all(allReq)
+    if(payload && payload.length >0){
+        for(let each of payload){
+          const url = `https://www.omdbapi.com/?i=${each}&apikey=a07e499a`
+          allReq.push(call(getData,url))
+        }
+        response = yield all(allReq)
 
-    let allRes = []
-    for(let each of response){
-      if(each.data && each.data.Response){
+        let allRes = []
+        for(let each of response){
+          if(each.data && each.data.Response){
 
-        allRes.push({
-          Title:each.data.Title, Poster:each.data.Poster,
-          Year:each.data.Year, imdbID: each.data.imdbID
-        })
+            allRes.push({
+              Title:each.data.Title, Poster:each.data.Poster,
+              Year:each.data.Year, imdbID: each.data.imdbID
+            })
+          }else{
+            allRes.push({Error:"Some Error"})
+          }
+        }
+
+      if("Error" in allReq[0]){
+        yield put({
+          type: ACTIONS.MOVIES.GET_MOVIE_WITH_ID_FAIL,
+          payload: allRes
+        });
       }else{
-        allRes.push({Error:"Some Error"})
+        yield put({
+          type: ACTIONS.MOVIES.GET_MOVIE_WITH_ID_SUCCESS,
+          payload: allRes
+        });
       }
-    }
-
-    if("Error" in allReq[0]){
-      yield put({
-        type: ACTIONS.MOVIES.GET_MOVIE_WITH_ID_FAIL,
-        payload: allRes
-      });
     }else{
       yield put({
-        type: ACTIONS.MOVIES.GET_MOVIE_WITH_ID_SUCCESS,
-        payload: allRes
+        type: ACTIONS.MOVIES.GET_MOVIE_WITH_ID_FAIL,
+        payload: "No data found!"
       });
     }
 
